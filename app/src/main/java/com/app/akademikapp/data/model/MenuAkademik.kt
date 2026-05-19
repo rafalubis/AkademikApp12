c5bbcf0c0aedc6e598f17766eb46f760e2f6a5da
rafalubis <2410511162@mahasiswa.upnvj.ac.id>
2026-05-19 19:26:59 +0700

    rafa

--- app/src/main/java/com/app/akademikapp/data/model/MenuAkademik.kt
+++ app/src/main/java/com/app/akademikapp/data/model/MenuAkademik.kt
@@ -2,5 +2,6 @@ package com.app.akademikapp.data.model
 
 data class MenuAkademik(
     val title: String,
-    val description: String
+    val description: String,
+    val iconResId: Int
 )
--- app/src/main/java/com/app/akademikapp/ui/main/AdminFragment.kt
+++ app/src/main/java/com/app/akademikapp/ui/main/AdminFragment.kt
@@ -1,27 +1,22 @@
 package com.app.akademikapp.ui.main
-
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.app.akademikapp.MainActivity
 import com.app.akademikapp.R
 import com.app.akademikapp.databinding.FragmentAdminBinding
-
 class AdminFragment : Fragment(R.layout.fragment_admin) {
-
     private var _binding: FragmentAdminBinding? = null
     private val binding get() = _binding!!
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         _binding = FragmentAdminBinding.bind(view)
-
-        binding.btnKembaliHomeAdmin.setOnClickListener {
-            (activity as? MainActivity)?.supportFragmentManager?.popBackStack()
+        binding.btnKelolaMahasiswa.setOnClickListener {
+            (activity as? MainActivity)?.replaceFragment(
+                DataMahasiswaApiFragment()
+            )
         }
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
